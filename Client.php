2ef58a44e7aa2c0d2a884a702d7926d2f8bb702f
valizada <vfizuli@gmail.com>
2015-08-27 17:53:19 +0500

    client class added with dummy methods, it will be used to create communication with the bot

---
+++ Client.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * This class will have all the methods needed
+ * for the communication with the bot
+ * User: valizada
+ * Date: 27/08/15
+ * Time: 17:48
+ */
+
+class Client {
+
+    public function getMe(){
+
+    }
+
+    public function sendMessage(){
+
+    }
+
+    public function forwardMessage(){
+
+    }
+
+    public function sendPhoto(){
+
+    }
+
+    public function sendAudio(){
+
+    }
+
+    public function sendDocument(){
+
+    }
+
+    public function sendSticker(){
+
+    }
+
+    public function sendVideo(){
+
+    }
+
+    public function sendVoice(){
+
+    }
+
+    public function sendLocation(){
+
+    }
+
+    public function sendChatAction(){
+
+    }
+
+    public function getUserProfilePhotos(){
+
+    }
+
+    public function getUpdates(){
+
+    }
+
+    public function setWebhook(){
+
+    }
+}
